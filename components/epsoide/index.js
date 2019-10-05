5d3623d55b549e361686605d8e2db6f34a2f8813
fireairforce <1344492820@qq.com>
2019-10-05 23:28:43 +0800

    feat:新增epsoide的布局

--- components/epsoide/index.js
+++ components/epsoide/index.js
@@ -11,6 +11,7 @@ Component({
      }
   },
   data: {
+      months: ['一月','二月','三月','四月','五月','六月','七月','八月','九月','十月','十一月','十二月'],
       year:0,
       month: '', 
       _index: '0'
@@ -19,6 +20,12 @@ Component({
 
   },
   attached: function(){
-   
+    let date = new Date();
+    let year = date.getFullYear();
+    let month = date.getMonth();
+    this.setData({
+      year,
+      month:this.data.months[month]
+    })
   }
 })
