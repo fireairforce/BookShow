4946e27088f84af12dee96d5887d70bfeeab8d52
fireairforce <1344492820@qq.com>
2019-10-08 14:06:36 +0800

    fix:修复音乐播放时的一个逻辑显示bug

--- components/classic/music/music.js
+++ components/classic/music/music.js
@@ -5,21 +5,38 @@ const mMgr = wx.getBackgroundAudioManager();
 
 Component({
   behaviors: [ classicBeh ],  
+  
   properties: {
      src: String,
      title: String
   },
+
   data: {
     pauseSrc: './images/player@waitting.png',
     playSrc: './images/player@playing.png',
     playing: false
   },
+
+  // 在wx-if触发的一个生命周期函数
+  attached: function () {
+    console.log(this.properties);
+    console.log('触发');
+    console.log(mMgr.src);
+       //  这里调用methods里面的方法不用使用this.methods
+       this._recoverStatus(); 
+  },
+  // 在切换组件的时候对之前的音乐播放器来个暂停
+  detached: function(event) {
+    //  mMgr.stop();
+   
+  },
+
   methods: {
     onPlay:function() {
       mMgr.title = this.properties.title;
       if(!this.data.playing){
         this.setData({
-          playing: !this.data.playing
+          playing: true
         })
         // 在设置音频播放的时候必须要设置一下title，必填
         mMgr.src = this.properties.src;
@@ -29,6 +46,22 @@ Component({
         })
         mMgr.pause();
       }
-    }
+    },
+
+    _recoverStatus : function () {
+      // 如果当前组件没有音乐播放
+      if(mMgr.paused) {
+        this.setData({
+          playing: false
+        })
+        return;
+      } 
+      if(mMgr.src == this.properties.src) {
+        this.setData({
+          playing: true
+        })
+      }
+     }
+
   }
 })
