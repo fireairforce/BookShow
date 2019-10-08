7a7b7fb6009311a57ca24290779fa82a0bd044e8
fireairforce <1344492820@qq.com>
2019-10-08 13:31:11 +0800

    feat:新增音乐播放API

--- components/classic/music/music.js
+++ components/classic/music/music.js
@@ -1,18 +1,34 @@
 import { classicBeh } from '../classic-beh';
+
+// 使用这个api来获得音频控制的api
+const mMgr = wx.getBackgroundAudioManager();
+
 Component({
   behaviors: [ classicBeh ],  
-  properities: {
-
+  properties: {
+     src: String,
+     title: String
   },
   data: {
-    pauseSrc: 'images/player@waitting.png',
-    playSrc: 'images/player@playing.png'
+    pauseSrc: './images/player@waitting.png',
+    playSrc: './images/player@playing.png',
+    playing: false
   },
-
-  /**
-   * 组件的方法列表
-   */
   methods: {
-
+    onPlay:function() {
+      mMgr.title = this.properties.title;
+      if(!this.data.playing){
+        this.setData({
+          playing: !this.data.playing
+        })
+        // 在设置音频播放的时候必须要设置一下title，必填
+        mMgr.src = this.properties.src;
+      } else {
+        this.setData({
+          playing: false
+        })
+        mMgr.pause();
+      }
+    }
   }
 })
