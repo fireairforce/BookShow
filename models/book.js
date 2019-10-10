a5b56d101f6a62609073506fb03e4db1382e3c80
fireairforce <1344492820@qq.com>
2019-10-10 12:41:40 +0800

    feat:新增详情书籍页面

--- models/book.js
+++ models/book.js
@@ -1,10 +1,37 @@
 import { HTTP } from '../utils/http-p';
 
 class BookModel extends HTTP {
+    // 获得书籍的列表装填
     getHotList() {
         return this.request({
             url:`book/hot_list`,
             method:'GET'
+        })
+    }
+    // 获得每个书籍的阅读人数
+    getMyBookCount() {
+        return this.request({
+            url: `/book/favor/count`
+        })
+    }
+
+    getDetail(id) {
+        return this.request({
+            url: `book/${id}/detail`
+        })
+    }
+   
+    // 获得喜欢的状态
+    getLikeStatus(id) {
+        return this.request({
+            url: `book/${id}/favor`
+        })
+    }
+
+    // 获得某个书籍的评论
+    getComment(id) {
+        return this.request({
+            url: `book/${id}/short_comment`
         })
     }
 }
--- pages/book-detail/book-detail.js
+++ pages/book-detail/book-detail.js
@@ -1,10 +1,37 @@
+import { BookModel } from '../../models/book';
+const bookmodel = new BookModel();
 Page({
   data: {
-
+    comments: [],
+    detail: null,
+    likeStatus: false,
+    likeCount: 0
   },
   onLoad: function (options) {
      const { id } = options;
+     const detail = bookmodel.getDetail(id);
+     const comments = bookmodel.getComment(id);
+     const likeStatus = bookmodel.getLikeStatus(id);
      
+     detail.then(res => {
+       console.log(res);
+       this.setData({
+          detail: res
+       })
+     })
+
+     comments.then(res => {
+       this.setData({
+          comments: res.comments
+       })
+     })
+
+     likeStatus.then(res => {
+       this.setData({
+         likeStatus: res.like_status,
+         likeCount: res.fav_nums
+       })
+     })
   },
   onReady: function () {
 
