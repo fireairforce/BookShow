284699ead55ce293b1f4c6843d569e087bbd970f
fireairforce <1344492820@qq.com>
2019-10-16 22:24:26 +0800

    feat:新增image-button组件

--- pages/my/my.js
+++ pages/my/my.js
@@ -7,16 +7,32 @@ Page({
 
   },
   onLoad: function (options) {
-    wx.getUserInfo({
-      // 只有用户授权之后才能获取到数据
-      success: res => {
-        console.log(res);
-      }
-    });
+    this.userAuthorized();
+    // wx.getUserInfo({
+    //   // 只有用户授权之后才能获取到数据
+    //   success: res => {
+    //     console.log(res);
+    //   }
+    // });
     // 弹窗
     // 询问是否授权
     // API
     // button 组件 UI 让用户主动点击button
+  },
+  userAuthorized () {
+    wx.getSetting({
+      success : res => {
+       if(data.authSetting['scope.userInfo']) {
+         wx.getUserInfo({
+          success: (res) => {
+            console.log(res);
+          }
+         })
+       } else {
+         console.log(`err`);
+       }
+      }
+    })
   },
   bindGetUserInfo (e) {
     // console.log(e.detail.userInfo)
