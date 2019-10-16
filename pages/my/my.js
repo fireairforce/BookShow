8bc8db6e9bf00cfa7c7796b3edd99b011f5652d7
fireairforce <1344492820@qq.com>
2019-10-16 22:13:19 +0800

    feat:完善用户授权功能

--- pages/my/my.js
+++ pages/my/my.js
@@ -7,6 +7,18 @@ Page({
 
   },
   onLoad: function (options) {
-    
+    wx.getUserInfo({
+      // 只有用户授权之后才能获取到数据
+      success: res => {
+        console.log(res);
+      }
+    });
+    // 弹窗
+    // 询问是否授权
+    // API
+    // button 组件 UI 让用户主动点击button
   },
+  bindGetUserInfo (e) {
+    // console.log(e.detail.userInfo)
+  }
 })
